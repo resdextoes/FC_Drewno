32368a9c347d85d02d4cd8e06c43d1b9e8124030
resdextoes <cieslakwojciech035@gmail.com>
2025-12-21 19:28:12 +0100

    Add files via upload

--- footer.js
+++ footer.js
@@ -9,14 +9,3 @@ document.addEventListener("DOMContentLoaded", () => {
         })
         .catch(err => console.error("Błąd ładowania footera:", err));
 });
-document.addEventListener("DOMContentLoaded", () => {
-    fetch("/header.html")
-        .then(response => response.text())
-        .then(data => {
-            const footer = document.getElementById("header");
-            if (footer) {
-                footer.innerHTML = data;
-            }
-        })
-        .catch(err => console.error("Błąd ładowania headera:", err));
-});
